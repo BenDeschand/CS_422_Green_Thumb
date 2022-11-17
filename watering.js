edbc4e0a84fde1d7d802ca35da06aed7a93c9ad8
otny12 <55300810+otny12@users.noreply.github.com>
2022-11-17 13:01:16 -0600

    Re organized columns, added some text info on what needs watering

--- watering.js
+++ watering.js
@@ -4,7 +4,15 @@ var today = '12';
 function changeDate(date){
     currentDay = date;
     console.log(currentDay);
-    $(infoCard).html("Now editing " + date);
+
+    if(currentDay==today){
+        $(infoCard).html("Today is 1/" + date +"<div class='card-header'>Needs Watering:</div>");
+        $(infoCard).append("<ul class='list-group list-group-flush'><li class='list-group-item list-group-item-info'>Sunflower</li>")
+    } else{
+       $(infoCard).html("Now editing 1/" + date);
+       $(infoCard).append("<div class='card-header'>Needed Watering:</div>")
+       $(infoCard).append("<ul class='list-group list-group-flush'><li class='list-group-item list-group-item-info'>Sunflower</li>")
+    }  
 }
 
 function setAction(action){
@@ -12,18 +20,18 @@ function setAction(action){
         const b = $('#'+currentDay)
         b.html("1/" +currentDay+" Watered")
         b.removeClass()
-        b.addClass("btn btn-info")
+        b.addClass("btn list-group-item-info border border-info")
     }
     if(action == 'skipped'){
         const b = $('#'+currentDay)
         b.html("1/"+currentDay+" Skipped")
         b.removeClass()
-        b.addClass("btn btn-warning")
+        b.addClass("btn list-group-item-warning border border-warning")
     }
     if(action == 'rained'){
         const b = $('#'+currentDay)
         b.html("1/"+currentDay+" Rained")
         b.removeClass()
-        b.addClass("btn btn-primary")
+        b.addClass("btn list-group-item-primary border border-primary")
     }
 }
